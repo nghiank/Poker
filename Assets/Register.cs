9f50be717de9995422798104396c69d6ad589386
Nghia Nguyen <nghiank@hotmail.com>
2018-01-05 18:04:02 -0800

    Hook up the RegisterButton click

--- Assets/Register.cs
+++ Assets/Register.cs
@@ -1,9 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Register : MonoBehaviour {
+	public GameObject email;
+	public GameObject password;
 
+	private string emailStr;
+	private string passwordStr;
+
+	public void RegsiterClick() {
+		print ("We are going to register for user :" + emailStr);
+	}
+		
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +21,7 @@ public class Register : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		emailStr = email.GetComponent<InputField> ().text;
+		passwordStr = password.GetComponent<InputField> ().text;
 	}
 }
