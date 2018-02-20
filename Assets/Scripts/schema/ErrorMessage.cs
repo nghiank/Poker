f1d954b553e3dd48b836635febed8c5a6bdcbbc7
Nghia Nguyen <nghiank@hotmail.com>
2018-02-19 22:34:01 -0800

    Added player placeholder for the scene

--- Assets/Scripts/schema/ErrorMessage.cs
+++ Assets/Scripts/schema/ErrorMessage.cs
@@ -17,21 +17,25 @@ public struct ErrorMessage : IFlatbufferObject
   public void __init(int _i, ByteBuffer _bb) { __p.bb_pos = _i; __p.bb = _bb; }
   public ErrorMessage __assign(int _i, ByteBuffer _bb) { __init(_i, _bb); return this; }
 
-  public string Error { get { int o = __p.__offset(4); return o != 0 ? __p.__string(o + __p.bb_pos) : null; } }
-  public ArraySegment<byte>? GetErrorBytes() { return __p.__vector_as_arraysegment(4); }
+  public ErrorCode Code { get { int o = __p.__offset(4); return o != 0 ? (ErrorCode)__p.bb.GetSbyte(o + __p.bb_pos) : ErrorCode.Unknown; } }
+  public string Error { get { int o = __p.__offset(6); return o != 0 ? __p.__string(o + __p.bb_pos) : null; } }
+  public ArraySegment<byte>? GetErrorBytes() { return __p.__vector_as_arraysegment(6); }
 
   public static Offset<ErrorMessage> CreateErrorMessage(FlatBufferBuilder builder,
+      ErrorCode code = ErrorCode.Unknown,
       StringOffset errorOffset = default(StringOffset)) {
-    builder.StartObject(1);
+    builder.StartObject(2);
     ErrorMessage.AddError(builder, errorOffset);
+    ErrorMessage.AddCode(builder, code);
     return ErrorMessage.EndErrorMessage(builder);
   }
 
-  public static void StartErrorMessage(FlatBufferBuilder builder) { builder.StartObject(1); }
-  public static void AddError(FlatBufferBuilder builder, StringOffset errorOffset) { builder.AddOffset(0, errorOffset.Value, 0); }
+  public static void StartErrorMessage(FlatBufferBuilder builder) { builder.StartObject(2); }
+  public static void AddCode(FlatBufferBuilder builder, ErrorCode code) { builder.AddSbyte(0, (sbyte)code, 0); }
+  public static void AddError(FlatBufferBuilder builder, StringOffset errorOffset) { builder.AddOffset(1, errorOffset.Value, 0); }
   public static Offset<ErrorMessage> EndErrorMessage(FlatBufferBuilder builder) {
     int o = builder.EndObject();
-    builder.Required(o, 4);  // error
+    builder.Required(o, 6);  // error
     return new Offset<ErrorMessage>(o);
   }
 };
